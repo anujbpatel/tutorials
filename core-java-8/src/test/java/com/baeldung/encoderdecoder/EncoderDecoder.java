5ab41107877eb635cb28ee9cc1613a75aee765f5
slavisa-baeldung <slavisa.baeldung@gmail.com>
2016-10-21 07:33:42 +0200

    BAEL-276 - URL encoding

--- core-java-8/src/test/java/com/baeldung/encoderdecoder/EncoderDecoder.java
+++ core-java-8/src/test/java/com/baeldung/encoderdecoder/EncoderDecoder.java
@@ -5,55 +5,72 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.UnsupportedEncodingException;
+import java.net.URL;
 import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class EncoderDecoder {
 
+    public static final String URL = "http://www.baeldung.com?key1=value+1&key2=value%40%21%242" +
+            "&key3=value%253";
+
     @Test
-    public void givenRequestParam_whenUTF8Scheme_thenEncode(){
+    public void givenURL_whenAnalyze_thenCorrect() throws Exception {
+
+        URL aURL = new URL(URL);
+
+        System.out.println("protocol = " + aURL.getProtocol());
+        System.out.println("authority = " + aURL.getAuthority());
+        System.out.println("host = " + aURL.getHost());
+        System.out.println("port = " + aURL.getPort());
+        System.out.println("path = " + aURL.getPath());
+        System.out.println("query = " + aURL.getQuery());
+        System.out.println("filename = " + aURL.getFile());
+        System.out.println("ref = " + aURL.getRef());
+    }
+
+    @Test
+    public void givenRequestParam_whenUTF8Scheme_thenEncode() throws Exception {
         Map<String, String> requestParams = new HashMap<>();
         requestParams.put("key1", "value 1");
         requestParams.put("key2", "value@!$2");
         requestParams.put("key3", "value%3");
 
-        StringBuilder requestParamsString = new StringBuilder();
-        requestParams.forEach((key, value) -> {
-            try {
-                requestParamsString
-                        .append(key)
-                        .append("=")
-                        .append(URLEncoder.encode(value,
-                                StandardCharsets.UTF_8.toString()))
-                        .append("&");
-            } catch (UnsupportedEncodingException e) {
-                e.printStackTrace();
-            }
-        });
+        String encodedQuery = requestParams.keySet().stream()
+          .map(key -> key + "=" + encodeValue(requestParams.get(key)))
+          .collect(Collectors.joining("&"));
         String encodedURL = "http://www.baeldung.com?" +
-                requestParamsString
-                        .substring(0,
-                                requestParamsString.length()-1);
+                encodedQuery;
 
         Assert.assertThat(
-                "http://www.baeldung.com?key1=value+1&key2=value%40%21%242" +
-                        "&key3=value%253", CoreMatchers.is(encodedURL));
+                URL, CoreMatchers.is(encodedURL));
+    }
+
+    private String encodeValue(String value) {
+        String encoded = null;
+        try {
+            encoded = URLEncoder.encode(value, StandardCharsets.UTF_8.toString());
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        return encoded;
     }
 
     @Test
     public void givenRequestParam_whenUTF8Scheme_thenDecodeRequestParams() throws UnsupportedEncodingException {
-        String encodedURL = "http://www.baeldung.com?key1=value+1"+
-                "&key2=value%40%21%242&key3=value%253" ;
+        String encodedURL = "http://www.baeldung.com?key1=value+1" +
+                "&key2=value%40%21%242&key3=value%253";
         StringBuilder requestParamsString =
                 new StringBuilder("http://www.baeldung.com?");
 
         String encodedRequestParamsString =
                 encodedURL.substring(encodedURL.indexOf("?") + 1);
         String keyValuePairs[] = encodedRequestParamsString.split("&");
-        for(String keyValuePair: keyValuePairs) {
+        for (String keyValuePair : keyValuePairs) {
             String keyAndValue[] = keyValuePair.split("=");
             requestParamsString
                     .append(keyAndValue[0])
@@ -62,23 +79,23 @@ public class EncoderDecoder {
                             StandardCharsets.UTF_8.toString()))
                     .append("&");
         }
-        Assert.assertEquals("http://www.baeldung.com?key1=value 1"+
+        Assert.assertEquals("http://www.baeldung.com?key1=value 1" +
                         "&key2=value@!$2&key3=value%3",
                 requestParamsString.substring(0,
-                        requestParamsString.length()-1));
+                        requestParamsString.length() - 1));
     }
 
     @Test
     public void givenEncodedURL_whenWrongScheme_thenDecode() throws UnsupportedEncodingException {
-        String encodedURL = "http://www.baeldung.com?key1=value+1"+
-                "&key2=value%40%21%242&key3=value%253" ;
+        String encodedURL = "http://www.baeldung.com?key1=value+1" +
+                "&key2=value%40%21%242&key3=value%253";
         StringBuilder requestParamsString =
                 new StringBuilder("http://www.baeldung.com?");
 
         String encodedRequestParamsString =
                 encodedURL.substring(encodedURL.indexOf("?") + 1);
         String keyValuePairs[] = encodedRequestParamsString.split("&");
-        for(String keyValuePair: keyValuePairs) {
+        for (String keyValuePair : keyValuePairs) {
             String keyAndValue[] = keyValuePair.split("=");
             requestParamsString
                     .append(keyAndValue[0])
@@ -87,8 +104,8 @@ public class EncoderDecoder {
                             StandardCharsets.UTF_16.toString()))
                     .append("&");
         }
-        Assert.assertFalse(("http://www.baeldung.com?key1=value 1"+
+        Assert.assertFalse(("http://www.baeldung.com?key1=value 1" +
                 "&key2=value@!$2&key3=value%3")
-                        .equals(requestParamsString));
+                .equals(requestParamsString));
     }
 }
